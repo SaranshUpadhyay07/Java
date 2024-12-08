47942e66fc9bea760657a0c675f8847ac3ca758c
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2024-12-08 16:29:42 +0530

    The use of if statement

--- SecondClass.java
+++ SecondClass.java
@@ -1,5 +1,12 @@
 public class SecondClass {
     public static void main(String[] args) {
         System.out.println("Hello, Saransh");
+
+        boolean isAlien = false;
+        if(isAlien == false) {
+            System.out.println("Not an Alien");
+            System.out.println("And i am scared of aliens");
+        }
+
     }
 }
