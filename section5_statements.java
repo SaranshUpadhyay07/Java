4b7d63ff14de740ce45c03aae3b4f4eda46e5b80
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2024-12-11 20:55:16 +0530

    section 5 Statements, Whitespaces, Indentation

--- section5_statements.java
+++ section5_statements.java
@@ -1,2 +1,13 @@
 public class section5_statements {
+    public static void main(String[] args) {
+        int myvariable = 50;
+
+        myvariable++; // java doesn't care for the no. of whitespace
+        myvariable --;
+
+        System.out.println("This is test");
+        System.out.println("This is " +
+                "another " +
+                "still more");
+    }
 }
