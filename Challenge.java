d08cb74b5791412d551aa8ca8f0d5e173159b708
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2024-12-11 20:44:01 +0530

    Challenge.java and Section5_Keywords.java added

--- Challenge.java
+++ Challenge.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Challenge {
+    public static void main(String[] args) {
+        double a = 20.00;
+        double b = 80.00;
+        double c = (a+b) * 100.00;
+        double d = c % 40.00;
+        boolean Check = (d == 0.00) ? true : false;
+        System.out.println(Check);
+        if (Check==false) {
+            System.out.println("Got some remainder");
+        }
+        else
+        {
+            System.out.println("No remainder");
+        }
+    }
 }
---
+++ Section5_KeywordandExpression.java
@@ -0,0 +1,15 @@
+public class Section5_KeywordandExpression {
+    public static void main(String[] args) {
+        double kilometers = (100 * 1.609344);
+
+        int high_score = 50;
+
+        if(high_score > 25) {
+            high_score = 1000 + high_score;
+        }
+        int health = 100;
+        if((health > 50) && (high_score > 100)) {
+            high_score = high_score - 1000;
+        }
+    }
+}
