e761ae2a6301846fcc5bba212c45e6f598c0c572
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2024-12-14 00:34:56 +0530

    Challenge: Method Overloadig

---
+++ Challenge/challenge4.java
@@ -0,0 +1,19 @@
+// method overload
+package Challenge;
+
+public class challenge4 {
+    public static void main(String[] args) {
+        System.out.println("The value in centimeters is "+ toCenti(6,11));
+        System.out.println("The value in centimeters is "+ toCenti(12));
+
+    }
+    public static double toCenti(int feet, int inches){
+        feet *= 12;
+        double value = (double)(feet+inches)*2.54; // you could also do this by calling the inches function in the return
+        return value;
+    }
+    public static double toCenti(int feet){
+        double value = feet*30.48;
+        return value;
+    }
+}
