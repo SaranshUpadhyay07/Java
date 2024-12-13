a535ae94c9d35225f3484a287f9f0488cae3f8ec
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2024-12-14 01:53:58 +0530

    Validation added in Challenge 5

--- Challenge/Challenge5.java
+++ Challenge/Challenge5.java
@@ -2,7 +2,9 @@ package Challenge;
 
 public class Challenge5 {
     public static void main(String[] args) {
-        System.out.println(getDurationString(344449));
+        System.out.println(getDurationString(-344449));
+        System.out.println(getDurationString (-65, 54));
+        System.out.println(getDurationString (449, 120));
 
     }
     public static String getDurationString(int seconds){
@@ -23,6 +25,10 @@ public class Challenge5 {
         if((minutes>=0) && (seconds>=0 && seconds<=59)){
             hours = minutes / 60;
             minutes = minutes % 60;
+        }
+        else
+        {
+            return "No valid value";
         }
         return hours + "h " + minutes + "m ";
 
