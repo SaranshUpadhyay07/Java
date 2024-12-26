061710920d0ed6086ddaf3eec231e3b8b6bfcc8f
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2024-12-26 18:25:14 +0530

    Aise hi

--- section_6_5_TheWhileLoop_DoWhile.java
+++ section_6_5_TheWhileLoop_DoWhile.java
@@ -1,21 +1,30 @@
 public class section_6_5_TheWhileLoop_DoWhile {
     public static void main(String[] args){
-        int j =1;
-        boolean isready = false;
-        while(true)
-        {
-            if(j>5)
+//        int j =1;
+//        boolean isready = false;
+//        while(true)
+//        {
+//            if(j>5)
+//            {
+//                break;
+//            }
+//            System.out.println(j);
+//            j++;
+//        }
+//
+//        do
+//        {
+//            System.out.println(j);
+//            j++;
+//        }while(isready);
+        int number = 0;
+        while(number < 50){
+            number+=5;
+            if(number% 25 == 0)
             {
-                break;
+                continue;
             }
-            System.out.println(j);
-            j++;
+            System.out.print(number+"_");
         }
-
-        do
-        {
-            System.out.println(j);
-            j++;
-        }while(isready);
     }
 }
