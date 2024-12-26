a7f1fdf348d439607b82911cac018610c176a467
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2024-12-26 18:21:54 +0530

    Do while loop

--- section_6_5_TheWhileLoop_DoWhile.java
+++ section_6_5_TheWhileLoop_DoWhile.java
@@ -1,6 +1,7 @@
 public class section_6_5_TheWhileLoop_DoWhile {
     public static void main(String[] args){
         int j =1;
+        boolean isready = false;
         while(true)
         {
             if(j>5)
@@ -10,5 +11,11 @@ public class section_6_5_TheWhileLoop_DoWhile {
             System.out.println(j);
             j++;
         }
+
+        do
+        {
+            System.out.println(j);
+            j++;
+        }while(isready);
     }
 }
