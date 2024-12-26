093911132a75e6b3e92ff8e650915c644d573fd5
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2024-12-26 18:18:46 +0530

    If statement used

--- section_6_5_TheWhileLoop_DoWhile.java
+++ section_6_5_TheWhileLoop_DoWhile.java
@@ -1,8 +1,12 @@
 public class section_6_5_TheWhileLoop_DoWhile {
     public static void main(String[] args){
         int j =1;
-        while(j<=5)
+        while(true)
         {
+            if(j>5)
+            {
+                break;
+            }
             System.out.println(j);
             j++;
         }
