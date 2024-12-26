bb5acc1a72da7b43db99ce62595857b54242a889
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2024-12-26 18:15:40 +0530

    While loop basic

---
+++ section_6_5_TheWhileLoop_DoWhile.java
@@ -0,0 +1,10 @@
+public class section_6_5_TheWhileLoop_DoWhile {
+    public static void main(String[] args){
+        int j =1;
+        while(j<=5)
+        {
+            System.out.println(j);
+            j++;
+        }
+    }
+}
