7826f4a08b7d39087cdaea85c842338da50fff0a
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2024-12-28 03:03:13 +0530

    Section 6: Parsing Values and Reading Input

---
+++ section6_6_ParsingValuesandReadingInput.java
@@ -0,0 +1,11 @@
+public class section6_6_ParsingValuesandReadingInput {
+    public static void main(String[] args) {
+        int current_year = 2024;
+        String userDateofBirth = "1999";
+        int dateofBirth = Integer.parseInt(userDateofBirth);
+        System.out.println("Age = "+ (current_year - dateofBirth));
+        String usersAgeWithPartialYear = "24.5";
+        double ageWithPartialYear = Double.parseDouble(usersAgeWithPartialYear);
+        System.out.println("The person says is "+ageWithPartialYear+ " years old");
+    }
+}
