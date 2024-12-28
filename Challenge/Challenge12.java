2c6f1d4bf9c9d55282b43b105cfb9ab0428e128e
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2024-12-28 15:10:02 +0530

    Challenge 12: Takng Input to take only valid integer

--- Challenge/Challenge12.java
+++ Challenge/Challenge12.java
@@ -1,4 +1,25 @@
 package Challenge;
-
+import java.util.Scanner;
 public class Challenge12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i =1;
+        do{
+
+            System.out.println("Enter the number #"+i);
+            try{
+                String number = sc.nextLine();
+                int num = Integer.parseInt(number);
+                i++;}
+            catch(NumberFormatException e){System.out.println("Kindly Enter a Correct number");}
+        }while(i<=5);
+        do{
+            try{
+            String number = System.console().readLine("Enter the number #"+i+" ");
+            int num = Integer.parseInt(number);
+            i++;}
+            catch(NumberFormatException e){System.out.println("Kindly Enter a Correct number");}
+        }while(i<11);
+    }
+
 }
