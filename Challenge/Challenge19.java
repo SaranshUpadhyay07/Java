a95dd08df1c55a9e8a92acb6f92f4756631b3671
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2025-02-08 23:57:41 +0530

    Section 8: Array part1

--- Challenge/Challenge19.java
+++ Challenge/Challenge19.java
@@ -2,7 +2,7 @@ package Challenge;
 
 public class Challenge19 {
     public static void main(String[] args) {
-        Car ferrari = new HybridCar("2022 Blue 296 GTS", 15.4,6,2,2.5);
+        Car ferrari = new GasPowerCar("2022 Blue 296 GTS", 15.4,6,2);
         ferrari.drive();
         ferrari.startEngine();
     }
---
+++ Section_9_Arrays.java
@@ -0,0 +1,16 @@
+public class Section_9_Arrays {
+    public static void main(String[] args) {
+        int[] integerArray;
+        int integerArray2 [];
+
+        int[] integerArray3 = new int[10];
+
+        integerArray3[9]=10;
+
+        int [] integerArray4 = new int[]{1,2,3,4,5,6}; // The array initializer
+        int [] integerArray5 ={1,2,3,4,5,6};// The array initializer as an anonymous array
+
+        System.out.println(integerArray5[2]);
+        System.out.println(integerArray4.length);
+    }
+}
