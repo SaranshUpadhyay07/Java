73240b141fe6fb9b607f02b104d4552c39cd0459
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2025-02-09 17:26:12 +0530

    h

--- Section_9_SearchingAndMatching.java
+++ Section_9_SearchingAndMatching.java
@@ -14,7 +14,7 @@ public class Section_9_SearchingAndMatching {
         int[] s1 ={1,2,3,4,5};
         int[] s2 ={1,2,3,4,5};
 
-        if(Arrays.equals(s1,s2)){ //Take the order of the items and the array length 
+        if(Arrays.equals(s1,s2)){ //Take the order of the items and the array length
             System.out.println("Equal");
         }else{
             System.out.println("Not Equal");
