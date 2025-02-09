20fa074007498f0dc1844712443c529472d4359e
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2025-02-09 17:25:11 +0530

    Section 9: Searching and Matching the Arrays

--- Section_9_SearchingAndMatching.java
+++ Section_9_SearchingAndMatching.java
@@ -1,2 +1,23 @@
+import java.util.Arrays;
+
 public class Section_9_SearchingAndMatching {
+    public static void main(String[] args) {
+
+        String[]sArray ={"Able", "gote"};
+        Arrays.sort(sArray);
+        System.out.println(Arrays.toString(sArray));
+
+        if(Arrays.binarySearch(sArray, "gote")>=0){
+            System.out.println("Found it");
+        }
+
+        int[] s1 ={1,2,3,4,5};
+        int[] s2 ={1,2,3,4,5};
+
+        if(Arrays.equals(s1,s2)){ //Take the order of the items and the array length 
+            System.out.println("Equal");
+        }else{
+            System.out.println("Not Equal");
+        }
+    }
 }
