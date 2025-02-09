e6875660d9d6068225ace24323c7d5131ae90a8f
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2025-02-09 17:45:03 +0530

    Challenge 20 : Array in descending order;

--- Challenge/Challenge20.java
+++ Challenge/Challenge20.java
@@ -1,4 +1,26 @@
 package Challenge;
 
+import java.util.*;
+
 public class Challenge20 {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int [] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+
+        System.out.println(Arrays.toString(array));
+
+        Arrays.sort(array);
+
+        for(int i=0;i<array.length/2;i++){
+            int temp = array[i];
+            array[i] = array[array.length-1-i];
+            array[array.length-1-i] = temp;
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
 }
